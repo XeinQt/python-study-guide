cdd109d5bc7cf4d5e26d7a67446bab95fe10a4e0
My Name <ricoalentijo4@gmail.com>
2026-01-06 07:22:47 +0800

    fundamentals

---
+++ app.py
@@ -0,0 +1,28 @@
+#comments ni
+# print("nunbers ni", 1213, "hotssg")
+
+#variables
+# x = 3
+# y = "asfsdaf"
+# # total = x + y;
+# # print(total)
+# print(type(x))
+# print(type(y))
+
+
+#assigning ultiple values
+fruits = ["orange","apple", "grapes"]
+
+#puts all the value
+# x = y = z = fruits;
+
+#assigned value for each
+# x,y,z = fruits;
+# print(x);
+
+#printing multiples values
+# x = "hello lord"
+# y = "mangtras"
+# z = "gotsdfs"
+
+# print(x,y,z)
