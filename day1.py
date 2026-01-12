97d5ba9421fae61fefb6fb92b73c8dd7fbfdb6c8
My Name <ricoalentijo4@gmail.com>
2026-01-12 08:43:05 +0800

    day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,4 @@
 # done the how to run and install the python 
+#done the understanding how python works and where the python use. 
+#i do
+# Install Python, create a file, display a message, add comments, run via IDE & terminal 
