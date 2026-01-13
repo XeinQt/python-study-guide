1d87a75face1b413598e5991f13ccf3bab5c9e81
My Name <ricoalentijo4@gmail.com>
2026-01-13 16:20:06 +0800

    day 2

---
+++ day2.py
@@ -0,0 +1,317 @@
+#comments ni
+# print("nunbers ni", 1213, "hotssg")
+
+#variables
+# x = 3
+# y = "asfsdaf"
+# # total = x + y;
+# # print(total)
+# print(type(x))
+# print(type(y))
+
+
+#assigning ultiple values
+fruits = ["orange","apple", "grapes"]
+
+#puts all the value
+# x = y = z = fruits;
+
+#assigned value for each
+# x,y,z = fruits;
+# print(x);
+
+#printing multiples values
+# x = "hello lord"
+# y = "mangtras"
+# z = "gotsdfs"
+
+# print(x,y,z)
+
+
+#functiions
+
+# x = "awsome";
+
+# def myFunction():
+#     x = "IS GOOD"
+#     print("python is good " + x)
+
+
+# myFunction()
+
+
+# def addFunction(x, y):
+#     print(x + y);
+
+# addFunction(5, 5)
+
+
+# def globalTotal():
+#     global x
+#     x = 0
+
+# def addNumber(num1, num2):
+#     global x
+#     x = x + (num1 + num2)
+
+# def minusNumbers(num1, num2):
+#     global x
+#     x = x + (num1 - num2)
+
+# globalTotal()
+# addNumber(5, 5)
+# minusNumbers(5, 2)
+# print(x)
+
+# x = 5;
+# x = "Hello World"
+# x = 20.5
+# x = 1j
+# x = ["apple", "banana", "cherry"]
+# x = ("apple", "banana", "cherry")
+# x = range(6)
+# x = {"name" : "John", "age" : 36}
+# x = {"apple", "banana", "cherry"}
+# x = frozenset({"apple", "banana", "cherry"})
+# x = True
+# x = b"Hello"
+# x = bytearray(5)
+# x = memoryview(bytes(5))
+# x = None
+
+# print(type(x))
+
+
+
+#python numbers
+
+# x = 4
+# y = 2.54
+# z = 1j
+
+# print(type(x))
+
+# total = x + z
+
+# print(total)
+
+#coverting numbers
+
+# a = float(x)
+# b = int(y)
+# c = complex(x)
+
+
+# print((c))
+
+#use the random numbers
+# import random
+
+# print(random.randrange(1,10));
+
+#casting
+# x = int(1);
+# y = int("2")
+
+# a = str(x)
+
+# print(type(y))
+# print(type(a))
+
+#casting is coverting into other data types 
+
+#string
+# a = """"lorem impsum dolor hotdog cheesedog manok na adobo""";
+
+# print(a)
+
+# name = str("Rico")
+
+# #advance looping
+# for x in name:
+#     print(x)
+
+
+# #length
+# a = "Hello, World"
+# print(len(a))
+
+# #in length, the space still counted!
+
+# #printing base on the text
+
+# print("hello" in a)
+
+# #advance in if else
+# if "Hello" in a:
+#     print("naay Hello")
+# else:
+#     print("walay hello")
+
+# if "Hello" not in a:
+#     print("walay helo")
+# else:
+#     print("naay hello")
+
+
+#slicing the text
+# b = "Hello, world!"
+# print(b[2:5])
+# print(b[:5])
+# print(b[6:])
+
+#modify string
+# a = "Hello"
+
+# print(a.upper());
+# print(a.lower());
+# #strip is cutt
+# \
+# ing the space in the 1st and the last
+# print(a.strip());
+
+
+# print(a.replace("Hello", "World"));
+
+# b = "Hello,hotdog"
+# print(b.split(","));
+
+#concat
+# x = "World"
+# y = "hello"
+
+# total = x + "" + y
+# print(total)
+
+
+#format string
+# b = 43
+# h = "hotdog"
+# se = f"the price is {b} of the bag same as the {h}"
+
+
+# price = float(45.31231)
+# s = f"The price of the bag is {price:.2f}"
+# print(s)
+
+#escape characters
+# j = "The dog's name is \"Vbooo\" "
+# print(j)
+
+
+#boolean
+
+# if(""):
+#     print("true")
+# else: 
+#     print("false")
+
+# def myFunction():
+#     return True;
+
+# if myFunction():
+#     print("yes")
+# else:
+#     print("no")
+
+
+#operators
+
+# sum = 1 + 1;
+# sum = 1 * 1;
+# sum = 1 / 1;
+# sum = 1 - 1;
+# print(sum)
+
+# sum = 0
+
+# sum += 10
+# sum -= 10
+# sum *= 10
+# sum /= 10
+# print(sum)
+
+
+#comparison
+
+# ==
+# !=
+# >
+# <
+# <=
+# >=
+
+
+#logical
+
+# and, or and not
+
+# x = 5;
+# y = 2
+
+# if not(x == 5 and y == 2):
+#     print("True")
+# else :
+#     print("false")
+
+
+# identify operators
+# app = ["apple"]
+# ban = ["banana"]
+
+# print(app is not ban)
+
+#membership operators
+
+# fruits = ["Banana", "apple"]
+
+# search = "Banan1a"
+
+# if search in fruits:
+#     print(search)
+# else:
+#     print("Not found!")
+
+
+#to sum up all the operators, it uses all the same in programming language but not same syntax then the python is easier
+
+
+#array or list
+
+
+
+#accessing the list 
+# print(mylist[0])
+# print(mylist[-1])
+# print(mylist[2:5])
+# print(mylist[:4])
+# print(mylist[2:])
+
+# search = "hotdog"
+
+# if search in mylist:
+#     print(f"{search} is in the list")
+# else:
+#     print(f"{search} not found!")
+
+#changing the list or array
+# mylist[1] = "hotdog"
+
+# mylist[:5] = ["melon"]
+
+# print(mylist)
+
+#adding in the list
+
+#creating a list
+mylist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+# exte = ["gwpo", "rico"]
+# #adding this in the last of the list
+# mylist.append("hotdohg")
+# #adding in the specific list or index
+# mylist.insert(0, "manika")
+# mylist.extend(exte)
+# print(mylist)
+
+
